d2ac41c15bcb6064cd273c15d06016d0ffe6892a
razibhosen24 <razibhosen24@gmail.com>
2025-04-11 02:17:24 +0600

    loading animation update

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ function videoconAnimation(){
 };
 videoconAnimation();
 // end of the function
+
+//Loading animation
+function loadingAnimation(){
+  //h1 animation
+  gsap.from("#page1 h1", {
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.2,
+  });
+  //video container animation
+  gsap.from("#page1 #video-container", {
+    scale:0.9,
+    opacity:0,
+    delay:1.3,
+    duration:0.5,
+  });
+};
+loadingAnimation();
+// end of the function
